ab1d9b67edc549a1067ca050c6f85f4398a3f77a
hikmdgj <hikmdgj@naver.com>
2023-04-03 15:18:35 +0900

    커밋4
    나누기 구현

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def sub_func(n1,n2) :
 
 def mul_func(n1,n2) :
     return n1*n2
+
+def dev_func(n1,n2) :
+    return n1/n2
+
 ## 전역 변수부(클래스 변수, 인스턴스 변수)
 num1, num2, result = 100, 200 ,0
 
@@ -21,3 +25,6 @@ print(num1,'-',num2, '=',result)
 
 result = mul_func(num1,num2)
 print(num1,'*',num2, '=',result)
+
+result = dev_func(num1,num2)
+print(num1,'/',num2, '=',result)
